6a76ebf7e33a8ae7e78877d403445bfb36060021
tommy w <tlwjr27@gmail.com>
2024-10-17 20:02:42 -0500

    Add project files.

---
+++ FinalProject/Models/ExerciseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FinalProject.Models
+{
+    public class ExerciseContext : DbContext
+    {
+        public ExerciseContext (DbContextOptions<ExerciseContext> options) : base(options) { }
+
+        DbSet<Exercises> Exercises { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FinalProject/Models/Exercises.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Models
+{
+    public class Exercises
+    {
+
+        public string ExerciseID { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Please enter a name for the exercise")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter a weight for the exercise")]
+        public int? Weight { get; set; }
+        [Required(ErrorMessage = "Please enter the amount of reps for the exercise")]
+        public int Reps { get; set; } 
+
+       
+    }
+}
