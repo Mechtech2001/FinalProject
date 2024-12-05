0e034496df2926c4a6d1225bfc57e8eaae81b421
cadenheidebrink <148292726+cadenheidebrink@users.noreply.github.com>
2024-12-05 15:33:28 -0600

    Update UserController.cs

--- FinalProject/Controllers/UserController.cs
+++ FinalProject/Controllers/UserController.cs
@@ -14,12 +14,10 @@ namespace FinalProject.Controllers
             return View();
         }
 
-        // Login method: stores username & premium status within session state
         public IActionResult Login(string username, string password, bool Premium)
         {
             Console.WriteLine("Login attempt received for Username: {0}, Premium: {1}", username, Premium);
 
-            // Find user by username
             var user = context.Users.FirstOrDefault(u => u.Username == username);
 
             if (user == null)
@@ -29,7 +27,6 @@ namespace FinalProject.Controllers
                 return View("~/Views/Home/Index.cshtml"); // Redisplay login view with an error
             }
 
-            // Check if the password matches
             if (user.Password != password)
             {
                 Console.WriteLine("Password mismatch for Username: {0}", username);
@@ -37,18 +34,15 @@ namespace FinalProject.Controllers
                 return View("~/Views/Home/Index.cshtmlx"); // Redisplay login view with an error
             }
 
-            // Successful login
             Console.WriteLine("Password match successful for {0}", username);
 
             HttpContext.Session.SetInt32("UserID", user.UserID);
 
-            // Store Premium in session
             HttpContext.Session.SetString("Premium", Premium.ToString());
             Console.WriteLine("Stored UserID and Premium status in session: {0}, {1}", user.UserID, Premium);
 
             Console.WriteLine("Redirecting to UserHome with UserID: {0}", user.UserID);
 
-            // Redirect to UserHome
             return RedirectToAction("UserHome", "Exercise", new { id = user.UserID });
         }
 
