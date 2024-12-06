b795690e6c5c3be7c27f375c1300bcf1e1c35f35
TatePadilla <TatePadilla@TATES-LAPTOP>
2024-12-05 18:59:53 -0600

    Adding Azure connection string and DB changes.

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -57,4 +57,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// Adding code to automatically migrate DB to Azure upon program start.
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<UserContext>();
+    context.Database.Migrate(); // Applies migrations automatically
+}
 app.Run();
